dc32d4ed71b21fe36c8db14b088c72a468b27a83
somnathkayal <skayal.bit@gmail.com>
2023-04-13 03:55:04 +0530

    indexed photos using hash

--- src/main/java/javastreams/MapStreamCode.java
+++ src/main/java/javastreams/MapStreamCode.java
@@ -10,7 +10,7 @@ import static java.util.Arrays.stream;
 
 public class MapStreamCode {
     public void creatingHash1(int[] a){
-//      third parameter of toMap() is to handle the collision of key
+//      third parameter of toMap() is to handle the collision of key, otherwise IllegalStateException would be thrown
 //      most important thing to note is boxed(). It's required because Arrays.stream(a) gives IntStream
 //      but in order to collect the integers in Map we need Stream<Integer>.
         Map<Integer, Integer> countMap = Arrays.stream(a)
@@ -29,7 +29,7 @@ public class MapStreamCode {
     }
 
     public void creatingHash3(int[] a){
-//      third parameter of toMap() is to handle the collision of key
+//      third parameter of toMap() is to handle the collision of key, otherwise IllegalStateException would be thrown
         Map<Integer, Integer> countMap = Arrays.stream(a)
                 .boxed()
                 .collect(Collectors.toMap(e -> e, v -> 1, (oldItem, newItem) -> oldItem+1));
@@ -37,7 +37,7 @@ public class MapStreamCode {
     }
 
     public void creatingHash4(List<Integer> a){
-//      third parameter of toMap() is to handle the collision of key
+//      third parameter of toMap() is to handle the collision of key, otherwise IllegalStateException would be thrown
 //      boxed() is NOT applicable here because List.stream is already giving Stream<Integer>.
         Map<Integer, Integer> countMap = a.stream()
                 .collect(Collectors.toMap(e -> e, v -> 1, Integer::sum));
@@ -45,10 +45,28 @@ public class MapStreamCode {
         countMap.entrySet().forEach(System.out::println);
     }
 
+    public Map<String, Photo> indexPhotoByName(List<Photo> photos){
+        // Not allowing to store multiple photos with same name, IllegalStateException would be thrown when collision occurs
+        Map<String, Photo> namePhotoMap = photos.stream()
+                .collect(Collectors.toMap(Photo::getName,
+                        Function.identity()));
+        namePhotoMap.entrySet().forEach(System.out::println);
+        return namePhotoMap;
+    }
+
+    public Photo findPhotoByName(List<Photo> photos, String target){
+        Map<String, Photo> index = indexPhotoByName(photos);
+
+        return index.getOrDefault(target, null);
+    }
+
     public static void main(String[] args) {
         int[] a = new int[]{4,5,2,7,4,4,2,7,7,7};
         MapStreamCode test = new MapStreamCode();
 //        test.creatingHash3(a);
-        test.creatingHash4(List.of(4,5,2,7,4,4,2,7,7,7));
+//        test.creatingHash4(List.of(4,5,2,7,4,4,2,7,7,7));
+//        test.indexPhotoByName(Photo.getSamplePhotos());
+        System.out.println(test.findPhotoByName(Photo.getSamplePhotos(), "abc"));
+        System.out.println(test.findPhotoByName(Photo.getSamplePhotos(), "abcd"));
     }
 }
